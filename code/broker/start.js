514fdfe92f3e3bbcd5b308bc88255f49fd04a775
Martin Frick <martin.frick@sap.com>
2023-07-31 15:00:48 +0200

    Update Service Broker bug

--- code/broker/start.js
+++ code/broker/start.js
@@ -5,7 +5,7 @@ import Broker from '@sap/sbf';
 
 // If VCAP_APPLICATION is defined, we are in a Cloud Foundry scenario
 // For local testing, the USER and PASSWORD are always injected via environment variables
-if(process.env.VCAP_APPLICATION && cds.env.profiles.find( p =>  p.includes("production"))){ 
+if(process.env.VCAP_APPLICATION && process.env.NODE_ENV === "production"){ 
     new Broker().start();
 }else{
     // In production Kyma scenarios and local testing, BROKER_USER and BROKER_PASSWORD are passed in env variables
