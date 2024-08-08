b1680c87eb915e68fba4a27b3762bfa0e76c6389
Alper <a.dedeoglu@sap.com>
2024-08-08 18:14:49 +0300

    emulation of vcap_services has been added

--- code/srv/srv/server.js
+++ code/srv/srv/server.js
@@ -1,10 +1,32 @@
 import cds from '@sap/cds';
 import cov2ap from '@cap-js-community/odata-v2-adapter'
 import ProvisioningService from './provisioning.js'
+import xsenv from '@sap/xsenv'
 
 cds.on('bootstrap', async (app) => {
     app.get('/healthz', (_, res) => res.status(200).send('OK'));
     app.use(cov2ap());
+
+    // Workaround to be removed for common db deployment on Kyma scenario.
+    // CAP Emulator does not function properly for HANA Common DB case and 
+    // it overwrites existing HANA container with common so deployment fails.
+    if (process.env.KYMA_DEPLOYMENT) {
+        console.log("Emulating the common db container")
+        var services = xsenv.getServices('/bindings', {
+            hana: { tag: 'hana' }
+        });
+        process.env.VCAP_SERVICES = JSON.stringify({
+            hana: [
+                {
+                    "name": "common-db",
+                    "tags":["hana"],
+                    "credentials": {
+                        ...services.hana
+                    },
+                }
+            ]
+        })
+    }
 });
 
 cds.on('served', async () => {
