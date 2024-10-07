84f9b58fbe36c589665f95452567d9b876730c2a
Alper <a.dedeoglu@sap.com>
2024-10-07 21:06:28 +0200

    add simple githubaction

--- code/broker/start.js
+++ code/broker/start.js
@@ -3,12 +3,34 @@
 
 import Broker from '@sap/sbf';
 
+// If this is a CI CD deployment we get broker credentials from env and hardcode the catalog.
+if (process.env.cicd) {
+    const catalog = `{
+        "services": [
+          {
+            "name": "susaas-api",
+            "description": "Sustainable SaaS API",
+            "bindable": true,
+            "plans": [
+              {
+                "name": "default",
+                "description": "Standard Plan",
+                "id": "57030d69-3e2d-4dd9-a138-21f16b542dff"
+              }
+            ],
+            "id": "6dec18a5-4742-450f-8840-ef9df0331b20"
+          }
+        ]
+      }`
+    let brokerConfig = { brokerCredentials: { [process.env["BROKER_USER"]]: process.env["BROKER_PASSWORD"] }, catalog: JSON.parse(catalog) }
+    new Broker(brokerConfig).start()
+}
 // If VCAP_APPLICATION is defined, we are in a Cloud Foundry scenario
 // For local testing, the USER and PASSWORD are always injected via environment variables
-if(process.env.VCAP_APPLICATION && process.env.NODE_ENV === "production"){ 
+else if (process.env.VCAP_APPLICATION && process.env.NODE_ENV === "production" && process.env.cicd !== true) {
     new Broker().start();
-}else{
+} else {
     // In production Kyma scenarios and local testing, BROKER_USER and BROKER_PASSWORD are passed in env variables
-    let brokerConfig = { brokerCredentials : { [process.env["BROKER_USER"]] : process.env["BROKER_PASSWORD"] }}    
+    let brokerConfig = { brokerCredentials: { [process.env["BROKER_USER"]]: process.env["BROKER_PASSWORD"] } }
     new Broker(brokerConfig).start()
 }
