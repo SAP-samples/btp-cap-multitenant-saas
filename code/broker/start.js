be64f9db7ec05dddc91e8a486bc450b402388eb9
Alper Dedeoglu <39748566+alperdedeoglu@users.noreply.github.com>
2025-01-15 13:23:46 +0100

    Fix/broker test upgrade (#91)
    * Broker catalog handling added to the test.
    @sap/sbf version upgrade
    
    * added broker init catalog step to gh actions
    
    * CI Deployment and stakeholder deployment for Broker is aligned
    
    * hash removed

--- code/broker/start.js
+++ code/broker/start.js
@@ -3,11 +3,14 @@
 
 import Broker from '@sap/sbf';
 
-// If this is a CI CD deployment we get broker credentials from env and hardcode the catalog.
- if (process.env.VCAP_APPLICATION && process.env.NODE_ENV === "production" && process.env.cicd !== true) {
+// If we are running as a CF deployment we get broker credentials and catalog from environment variables
+ if (process.env.VCAP_APPLICATION && process.env.NODE_ENV === "production") {
     new Broker().start();
 } else {
-    // In production Kyma scenarios, local testing and CI CD, BROKER_USER and BROKER_PASSWORD are passed in env variables
+    // In production Kyma scenarios, local testing BROKER_USER and BROKER_PASSWORD are passed in env variables
+    if(process.env.SBF_BROKER_CREDENTIALS) delete process.env.SBF_BROKER_CREDENTIALS
+    if(process.env.SBF_BROKER_CREDENTIALS_HASH) delete process.env.SBF_BROKER_CREDENTIALS_HASH
+    
     let brokerConfig = { brokerCredentials: { [process.env["BROKER_USER"]]: process.env["BROKER_PASSWORD"] } }
     new Broker(brokerConfig).start()
 }
