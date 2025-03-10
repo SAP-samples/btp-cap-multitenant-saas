4cc7abf506bdd53245a2e175b733a03ba37637f2
Alper Dedeoglu <39748566+alperdedeoglu@users.noreply.github.com>
2025-03-10 10:16:30 +0100

    Update subscription-test.sh

--- code/test/subscription-test.sh
+++ code/test/subscription-test.sh
@@ -180,7 +180,7 @@ check_subaccount_removal() {
 
     RESPONSE=$(btp --format json get accounts/subaccount "$SUBACCOUNT_GUID" 2>&1)
 
-    if echo "$response" | grep -q "\[Error: 20004/404\]"; then
+    if echo $RESPONSE | grep -q "\[Error: 20004/404\]"; then
       echo "Subaccount removed successfully."
       break
     else
